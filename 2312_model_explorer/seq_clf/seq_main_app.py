1073d250c48759af1facf497560fca97c9685d6f
id4thomas <id2thomas@gmail.com>
2023-12-16 23:24:15 +0900

    [edit] 2312_model_explorer - add latent_viz tab to seq_main_app

--- 2312_model_explorer/seq_clf/seq_main_app.py
+++ 2312_model_explorer/seq_clf/seq_main_app.py
@@ -1,10 +1,15 @@
 import gradio as gr
+
+## Import interfances
 from clf_perf import make_clf_perf_eval_interface
+from data_latent_viz import make_latent_viz_interface
 
 if __name__ == "__main__":
 	clf_perf_interface = make_clf_perf_eval_interface()
+	latent_viz_interface = make_latent_viz_interface()
+
 	demo = gr.TabbedInterface(
-		[clf_perf_interface], 
-		["Clf Performance"]
+		[clf_perf_interface, latent_viz_interface], 
+		["Clf Performance", "Latent Viz"]
 	)
 	demo.launch()
